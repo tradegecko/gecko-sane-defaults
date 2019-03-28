8a1a8fa079bc8b03b774e1be41002674641c4cbf
Bradley Priest <bradleypriest@gmail.com>
2019-03-28 14:14:40 +0800

    Little more

--- lib/gecko-rails-defaults/core_ext/frozen_string_literal.rb
+++ lib/gecko-rails-defaults/core_ext/frozen_string_literal.rb
@@ -1,18 +1,30 @@
 # Add # frozen_string_literal: true to all generated migrations
 # See https://gist.github.com/ta1kt0me/6a7058d16621785d4f7038bde6cd3b98
+module GeckoRailsDefaults
+  module AddFrozenStringLiteralComment
+    def add_frozen_string_literal_comment(dist)
+      if File.exist?(dist) && File.extname(dist) == '.rb'
+        File.open(dist, 'r') do |f|
+          body = f.read
 
-module AddFrozenStringLiteralComment
-  def add_frozen_string_literal_comment(dist)
-    if File.exist?(dist) && File.extname(dist) == '.rb'
-      File.open(dist, 'r') do |f|
-        body = f.read
-
-        File.open(dist, 'w') do |new_f|
-          new_f.write("# frozen_string_literal: true\n" + body)
+          File.open(dist, 'w') do |new_f|
+            new_f.write("# frozen_string_literal: true\n" + body)
+          end
         end
       end
     end
   end
-end
 
-Rails::Generators::Actions::CreateMigration.send :prepend, GeneratorPrepend
+  module GeneratorPrepend
+    include AddFrozenStringLiteralComment
+
+    def invoke!
+      res = super
+      add_frozen_string_literal_comment(existing_migration)
+      res
+    end
+  end
+
+  require "rails/generators/actions/create_migration"
+  Rails::Generators::Actions::CreateMigration.prepend GeneratorPrepend
+end
