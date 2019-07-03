13429da5804993335fc6fb13e22393dfdf9b43de
Bradley Priest <bradleypriest@gmail.com>
2019-07-03 10:01:15 +0800

    Bump the required ActiveJob monkeypatched versions

--- lib/gecko-sane-defaults/core_ext/activejob.rb
+++ lib/gecko-sane-defaults/core_ext/activejob.rb
@@ -3,7 +3,7 @@
 # No need to log error backtraces, they're already sent to Honeybadger
 # See https://github.com/rails/rails/blob/master/activejob/lib/active_job/logging.rb
 
-if ActiveJob::VERSION::STRING.in?(["5.1.6.2", "5.1.7", "5.2.2.1", "5.2.3"])
+if ActiveJob::VERSION::STRING.in?(["5.2.3", "6.0.0.rc1"])
   module ActiveJobLogExt
     def perform(event)
       job = event.payload[:job]
