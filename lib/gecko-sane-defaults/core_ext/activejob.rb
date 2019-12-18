639bc0d9b037bc22409b522224a1542cc1db3a0e
Bradley Priest <bradleypriest@gmail.com>
2019-12-18 15:06:13 +0800

    Update for 6.0.2

--- lib/gecko-sane-defaults/core_ext/activejob.rb
+++ lib/gecko-sane-defaults/core_ext/activejob.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if ActiveJob::VERSION::STRING.in?(["5.2.3", "6.0.0", "6.0.1"])
+if ActiveJob::VERSION::STRING.in?(["5.2.3", "6.0.2"])
   # No need to log error backtraces, they're already sent to Honeybadger
   # See https://github.com/rails/rails/blob/master/activejob/lib/active_job/log_subscriber.rb
   module ActiveJobLogExt
@@ -10,6 +10,10 @@ if ActiveJob::VERSION::STRING.in?(["5.2.3", "6.0.0", "6.0.1"])
       if ex
         error do
           "Error performing #{job.class.name} (Job ID: #{job.job_id}) from #{queue_name(event)} in #{event.duration.round(2)}ms: #{ex.class} (#{ex.message}):"
+        end
+      elsif event.payload[:aborted]
+        error do
+          "Error performing #{job.class.name} (Job ID: #{job.job_id}) from #{queue_name(event)} in #{event.duration.round(2)}ms: a before_perform callback halted the job execution"
         end
       else
         info do
